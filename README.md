f66edc1e3f87858e772307e6f10a9e281265fd88
B. Szonye <bszonye@gmail.com>
2025-04-12 02:28:12 -0700

    update README, trim unused colors

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Ready or Not 1.0.0
 better system buttons
+
+are your social policies ready for a change?
+is your resource allocation unlocked?
+
+Ready or Not switches the system buttons to full-color icons when their
+screens are unlocked.  it also turns the Social Policies button to gold
+during celebrations.
--- ui/sub-system-dock/bz-panel-sub-system-dock.js
+++ ui/sub-system-dock/bz-panel-sub-system-dock.js
@@ -1,48 +1,5 @@
 // color palette
 const BZ_COLOR = {
-    // game colors
-    silver: "#4c5366",  // = primary
-    bronze: "#e5d2ac",  // = secondary
-    primary: "#4c5366",
-    secondary: "#e5d2ac",
-    accent: "#616266",
-    accent1: "#e5e5e5",
-    accent2: "#c2c4cc",
-    accent3: "#9da0a6",
-    accent4: "#85878c",
-    accent5: "#616266",
-    accent6: "#05070d",
-    // alert colors
-    black: "#000000",
-    danger: "#af1b1c99",  // danger = militaristic 60% opacity
-    caution: "#cea92f",  // caution = healthbar-medium
-    note: "#ff800033",  // note = orange 20% opacity
-    // geographic colors
-    hill: "#ff800033",  // Rough terrain = orange 20% opacity
-    vegetated: "#aaff0033",  // Vegetated features = green 20% opacity
-    wet: "#55aaff66",  // Wet features = teal 60% opacity
-    road: "#e5d2accc",  // Roads & Railroads = bronze 80% opacity
-    // yield types
-    food: "#80b34d",        //  90°  40 50 green
-    production: "#a33d29",  //  10°  60 40 red
-    gold: "#f6ce55",        //  45°  90 65 yellow
-    science: "#6ca6e0",     // 210°  65 65 cyan
-    culture: "#5c5cd6",     // 240°  60 60 violet
-    happiness: "#f5993d",   //  30°  90 60 orange
-    diplomacy: "#afb7cf",   // 225°  25 75 gray
-    // independent power types
-    militaristic: "#af1b1c",
-    scientific: "#4d7c96",
-    economic: "#ffd553",
-    cultural: "#892bb3",
-    // relationship ring colors
-    friendly: "#e5d2ac",
-    hostile: "#af1b1c",
-    neutral: "#e0b96c",     //  40°  65 65 deep bronze
-    // highlight & shadow colors
-    light: "#fff6e5cc",     //  40° 100 95 pale bronze
-    shadow: "#00000080",
-    progress: "#e0b96c",    //  40°  65 65 deep bronze
     celebration: "#e8d37d", //  48°  70 70 gold
 };
 
